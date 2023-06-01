0330b86a4bbc52bb83b5d98b8f31674d6b8d3346
Михаил Каранинский <steammike23@gmail.com>
2023-06-01 05:39:26 +0500

    убрал лишний пробел

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('auth/', include('django.contrib.auth.urls')),
